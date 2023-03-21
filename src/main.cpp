8050b2fe4f78bd7b89380dd953e3b9764db9ef08
wangsp14 <wangsp14@midea.com>
2023-03-21 10:22:32 +0800

    LVGL

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,7 @@
 #include "../lib/TFT_ST7735/TFT_ST7735.h"
 #include <SPI.h>
 #include "../lib/TFT_ST7735/fonts/fluide_caps.h"
-// Color definitions
-#define BLUE 0x001F
-#define RED 0xF800
-#define GREEN 0x07E0
-#define CYAN 0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW 0xFFE0
+#include <lvgl.h>
 
 /*
 Teensy3.x and Arduino's
@@ -29,24 +23,72 @@ If you do not use reset, tie it to +3V3
 */
 
 TFT_ST7735 tft = TFT_ST7735(__CS, __DC, __RST);
+static const uint32_t screenWidth = 128;
+static const uint32_t screenHeight = 160;
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[screenWidth * 10];
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  int32_t x, y;
+  for (y = area->y1; y <= area->y2; y++)
+  {
+    for (x = area->x1; x <= area->x2; x++)
+    {
+      tft.drawPixel(x, y, *(uint16_t *)color_p);
+      color_p++;
+    }
+  }
+  lv_disp_flush_ready(disp);
+}
+
+void lv_example_btn_1(void)
+{
+  lv_obj_t *label;
+
+  lv_obj_t *btn1 = lv_btn_create(lv_scr_act());
+  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
+
+  label = lv_label_create(btn1);
+  lv_label_set_text(label, "Button");
+  lv_obj_center(label);
+
+  lv_obj_t *btn2 = lv_btn_create(lv_scr_act());
+  lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
+  lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
+  lv_obj_set_height(btn2, LV_SIZE_CONTENT);
+
+  label = lv_label_create(btn2);
+  lv_label_set_text(label, "Toggle");
+  lv_obj_center(label);
+}
 
 void setup()
 {
   tft.begin();
   tft.setRotation(2);
-  tft.setFont(&fluide_caps); // here we set the new font
-  tft.setTextColor(BLUE);
-  tft.print("HELLO,TEST");
-  tft.setCursor(0, 40);
-  tft.setFont(&internal); // this is the internal font, always loaded
-  tft.print("hello World");
-  // TFT 128*160
-  tft.drawPixel(127, 159, RED);
-  /*
-  The external fonts can use all features like scaling, color, etc.
-  */
+
+  lv_init();
+  lv_disp_draw_buf_init(&draw_buf, buf, NULL, screenWidth * 10);
+
+  /*Initialize the display*/
+  static lv_disp_drv_t disp_drv;
+  lv_disp_drv_init(&disp_drv);
+  /*Change the following line to your display resolution*/
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register(&disp_drv);
+
+  lv_example_btn_1();
 }
 
-void loop(void)
+void loop()
 {
+  // put your main code here, to run repeatedly:
+  lv_timer_handler(); /* let the GUI do its work */
+  delay(5);
 }
