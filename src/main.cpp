995cefb16e9258ec6432623e0ee20d11b3d57b53
wangsp14 <wangsp14@midea.com>
2023-03-21 10:40:53 +0800

    按键事件

--- src/main.cpp
+++ src/main.cpp
@@ -44,24 +44,29 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
   lv_disp_flush_ready(disp);
 }
 
-void lv_example_btn_1(void)
+static void event_cb(lv_event_t *e)
 {
-  lv_obj_t *label;
+  LV_LOG_USER("Clicked");
 
-  lv_obj_t *btn1 = lv_btn_create(lv_scr_act());
-  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
+  static uint32_t cnt = 1;
+  lv_obj_t *btn = lv_event_get_target(e);
+  lv_obj_t *label = lv_obj_get_child(btn, 0);
+  lv_label_set_text_fmt(label, "%" LV_PRIu32, cnt);
+  cnt++;
+}
 
-  label = lv_label_create(btn1);
-  lv_label_set_text(label, "Button");
-  lv_obj_center(label);
+/**
+ * Add click event to a button
+ */
+void lv_example_event_1(void)
+{
+  lv_obj_t *btn = lv_btn_create(lv_scr_act());
+  lv_obj_set_size(btn, 70, 60);
+  lv_obj_center(btn);
+  lv_obj_add_event_cb(btn, event_cb, LV_EVENT_CLICKED, NULL);
 
-  lv_obj_t *btn2 = lv_btn_create(lv_scr_act());
-  lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
-  lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
-  lv_obj_set_height(btn2, LV_SIZE_CONTENT);
-
-  label = lv_label_create(btn2);
-  lv_label_set_text(label, "Toggle");
+  lv_obj_t *label = lv_label_create(btn);
+  lv_label_set_text(label, "Click me!");
   lv_obj_center(label);
 }
 
@@ -83,7 +88,7 @@ void setup()
   disp_drv.draw_buf = &draw_buf;
   lv_disp_drv_register(&disp_drv);
 
-  lv_example_btn_1();
+  lv_example_event_1();
 }
 
 void loop()
